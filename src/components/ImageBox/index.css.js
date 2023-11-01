82eeb34ce0ba5c14727af65ec7300934bae45138
Shaon07 <alishaon77@gmail.com>
2023-11-01 23:01:01 +0600

    [add] imageBox

---
+++ src/components/ImageBox/index.css.js
@@ -0,0 +1,4 @@
+export const styles = {
+    imageBoxWrapper: `border-2 inline-block border-[#c7c7c7] rounded-md overflow-hidden relative`,
+    checkboxWrapper: `absolute top-0 right-0 left-0 bottom-0 bg-[#80808069] hover:bg-[#3b313169] cursor-pointer`
+}
--- src/container/HomeScreen.jsx
+++ src/container/HomeScreen.jsx
@@ -1,16 +1,21 @@
-import { Card, Divider } from 'antd'
-import React from 'react'
-import Header from '../components/Header'
+import { Card, Divider } from "antd";
+import React from "react";
+import Header from "../components/Header";
+import ImageBox from "../components/ImageBox";
 
 export default function HomeScreen() {
   return (
-    <div className='m-5'>
-      <Card bodyStyle={{padding:0}}>
+    <div className="m-5">
+      <Card bodyStyle={{ padding: 0 }}>
         <div>
-        <Header />
-        <Divider className='mt-0' />
+          <Header />
+          <Divider className="my-0" />
+        </div>
+
+        <div className="p-[25px] ">
+          <ImageBox />
         </div>
       </Card>
     </div>
-  )
+  );
 }
