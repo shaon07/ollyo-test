cccc195199d6d018e14e1ded97e1cb0053a0f264
Shaon07 <alishaon77@gmail.com>
2023-11-03 00:16:57 +0600

    [add] header delete button show hide on select image length check

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,22 +4,27 @@ import useImages from "../../hooks/useImages";
 import { styles } from "./index.css";
 
 export default function Header() {
-  const {selectImages,handleDeleteSelectImages} = useImages();
+  const { selectImages, handleDeleteSelectImages } = useImages();
   const selectedImageLen = selectImages.length;
   const isSelectedImage = selectedImageLen > 0 ? true : false;
 
   return (
     <div className={`${styles.headerWrapper}`}>
       <h2 className={`${styles.titleLeft}`}>
-        {isSelectedImage && <Checkbox  checked={true} />}
+        {isSelectedImage && <Checkbox checked={true} />}
         <span>
           {isSelectedImage ? selectedImageLen : ""}{" "}
           {isSelectedImage ? "Files Selected" : "Gallery"}
         </span>
       </h2>
-      <h4 className={`${styles.titleRight}`} onClick={handleDeleteSelectImages}>
-        Delete File{selectedImageLen > 1 ? "s" : ""}
-      </h4>
+      {isSelectedImage && (
+        <h4
+          className={`${styles.titleRight}`}
+          onClick={handleDeleteSelectImages}
+        >
+          Delete File{selectedImageLen > 1 ? "s" : ""}
+        </h4>
+      )}
     </div>
   );
 }
